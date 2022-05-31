7af26edfe0021e84350bd3e51fcde23e80a64594
Rody Kirwan <>
2022-05-31 16:09:55 +0100

    Test Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,26 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test Migration
-        </a>
-        <div>UPDATE App.js</div>
-        <div>Update after ejecting MFE</div>
-        <div>Updating all files in App</div>
-        <div>Test Diff</div>
-        <div>Change app.js</div>
-        <div>Test merge</div>
-        <div>{core1()}</div>
-        <div>test-script</div>
+        <div>Upfdate</div>
       </header>
     </div>
   );
