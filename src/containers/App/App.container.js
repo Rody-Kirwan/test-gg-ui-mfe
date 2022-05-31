ee5310503b40c3a90f0b2b3ad72abdc9231de631
Rody Kirwan <>
2022-05-31 15:28:53 +0100

    Change Header

--- src/containers/App/App.container.js
+++ src/containers/App/App.container.js
@@ -6,22 +6,7 @@ function AppContainer() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test Migration
-        </a>
-        <div>UPDATE App.js</div>
-        <div>Update after ejecting MFE</div>
-        <div>Updating all files in App</div>
-        <div>Test Diff</div>
+        <div>My New Header</div>
       </header>
     </div>
   );
